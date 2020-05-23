0711c7c0998ef14bb1141763a77ff1c096f1066c
Paul Schoepplenberg <paulschoepplenberg@Pauls-MacBook-Air.local>
2020-05-23 11:20:16 +0200

    Drawing single dots
    - added function to draw a little dot on mouseclick / mousedown

--- Paint_95.js
+++ Paint_95.js
@@ -1,6 +1,35 @@
-function createCanvas() {
-    let rows = 1000;
-    let colums = 1000;
+// window.onload = function init() {
+//     drawField = document.getElementById('myCanvas');
 
-    
+//     canvas.addEventListener('mousemove', function (evt) {
+//         mousePos = getMousePos(canvas, evt);
+//         var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+//         writeMessage(canvas, message);
+//     }, false);
+
+//     canvas.addEventListener('mousedown', function (evt) {
+//         mouseButton = evt.button;
+//         var message = "Mouse down button " + evt.button + " down at position: " + mousePos.x + ',' + mousePos.y;
+//         writeMessage(canvas, message);
+//     }, false);
+
+//     canvas.addEventListener('mouseup', function (evt) {
+//         var message = "Mouse up at position: " + mousePos.x + ',' + mousePos.y;
+//         writeMessage(canvas, message);
+//     }, false);
+// };
+
+function drawing(event) {
+    let paintCanvas = document.getElementById("paint-canvas");
+    let x = event.offsetX;
+    let y = event.offsetY;
+    console.log("x = " + x + " y = " + y);
+    let div = document.createElement("div");
+    paintCanvas.appendChild(div);
+    div.style.backgroundColor = "black";
+    div.style.position = "absolute";
+    div.style.left = x +'px';
+    div.style.top = y +'px';
+    div.style.height = "3px";
+    div.style.width = "3px";
 }
