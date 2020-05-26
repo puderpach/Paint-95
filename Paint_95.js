6e5ba81b560d9c0e2ad6a422a80ab17dd7d58145
Paul Schoepplenberg <paulschoepplenberg@Pauls-Air.fritz.box>
2020-05-26 21:44:23 +0200

    Avoiding double divs
    - changed: Event listener only in drawing div
    - added: Function to check if position was already clicked: But messed it up, not working right now.

--- Paint_95.js
+++ Paint_95.js
@@ -1,25 +1,45 @@
 let paintCanvas = document.getElementById("paint-canvas");
 let timer
+let paintedPixels = [];
 
 function drawing() {
-    document.addEventListener("mousemove", createDiv);
-    setInterval(createDiv(), 1);
+    document.querySelector("#paint-canvas").addEventListener("mousemove", createDiv);
+    setInterval(createDiv(), 0.00005);
 }
 
 function stopDrawing() {
-    document.removeEventListener("mousemove", createDiv);
+    document.querySelector("#paint-canvas").removeEventListener("mousemove", createDiv);
 }
 
 function createDiv() {
     let paintCanvas = document.getElementById("paint-canvas");
+    let position = {};
     let x = event.offsetX;
     let y = event.offsetY;
-    let div = document.createElement("div");
-    paintCanvas.appendChild(div);
-    div.style.backgroundColor = "black";
-    div.style.position = "absolute";
-    div.style.left = x +'px';
-    div.style.top = y +'px';
-    div.style.height = "3px";
-    div.style.width = "3px";
+    position.x = x;
+    position.y = y;
+    if (checkDiv(paintedPixels, x, y) === false) {
+        let div = document.createElement("div");
+        paintCanvas.appendChild(div);
+        div.style.backgroundColor = "black";
+        div.style.position = "absolute";
+        div.style.left = x + 'px';
+        div.style.top = y + 'px';
+        div.style.height = "3px";
+        div.style.width = "3px";
+        paintedPixels.push(position);
+    }
+}
+
+function checkDiv(array, valX, valY) {
+    console.log("test");
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+        if (array[i].x === valX && array[i].x === valY) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
