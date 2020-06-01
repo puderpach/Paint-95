2b9ae58fbebc550dddb7db1c5853b57279190835
Paul Schoepplenberg <paulschoepplenberg@Pauls-MacBook-Air.local>
2020-06-01 10:47:51 +0200

    Paint and erase now possible, current color shown and logic changed
    - added field that shows current color
    - added erase function
    - changed logic to enable various functions

--- Paint_95.js
+++ Paint_95.js
@@ -1,18 +1,23 @@
 let paintCanvas = document.getElementById("paint-canvas");
 let timer
 let color = "black";
-let tool
 
-function drawing() {
-    document.addEventListener("mousemove", createDiv);
-    setInterval(createDiv(), 0.00005);
+function paint() {
+    removeListener();
+    document.addEventListener("mousedown", startPaint);
+    document.addEventListener("mouseup", stopPaint);
 }
 
-function stopDrawing() {
-    document.removeEventListener("mousemove", createDiv);
+function startPaint() {
+    document.addEventListener("mousemove", paintDiv);
+    setInterval(paintDiv(), 0.00005);
 }
 
-function createDiv() {
+function stopPaint() {
+    document.removeEventListener("mousemove", paintDiv);
+}
+
+function paintDiv() {
     let paintCanvas = document.getElementById("paint-canvas");
     let x = event.clientX - 291;
     let y = event.clientY - 113;
@@ -26,6 +31,45 @@ function createDiv() {
     div.style.width = "3px";
 }
 
+function erase() {
+    removeListener();
+    document.addEventListener("mousedown", startErase);
+    document.addEventListener("mouseup", stopErase);
+}
+
+function startErase() {
+    document.addEventListener("mousemove", eraseDiv);
+    setInterval(eraseDiv(), 0.00005);
+}
+
+function stopErase() {
+    document.removeEventListener("mousemove", eraseDiv);
+}
+
+function eraseDiv() {
+    let paintCanvas = document.getElementById("paint-canvas");
+    let x = event.clientX - 291;
+    let y = event.clientY - 113;
+    let div = document.createElement("div");
+    paintCanvas.appendChild(div);
+    div.style.backgroundColor = "white";
+    div.style.position = "absolute";
+    div.style.left = x + 'px';
+    div.style.top = y + 'px';
+    div.style.height = "15px";
+    div.style.width = "15px";
+}
+
 function changeColor(newColor) {
+    let colorField = document.getElementById("current-color-box");
     color = newColor;
+    colorField.style.backgroundColor = newColor;
+}
+
+function removeListener() {
+    document.removeEventListener("mousedown", startPaint);
+    document.removeEventListener("mouseup", stopPaint);
+    document.removeEventListener("mousedown", startErase);
+    document.removeEventListener("mouseup", stopErase);
+    console.log("All erased")
 }
