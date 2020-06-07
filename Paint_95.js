56821f722d87f5b6d230b1ee2f06ff560288e171
Paul Schoepplenberg <paulschoepplenberg@Pauls-MacBook-Air.local>
2020-06-07 15:56:52 +0200

    Removed Event Listener and changed logic again

--- Paint_95.js
+++ Paint_95.js
@@ -1,63 +1,30 @@
 let paintCanvas = document.getElementById("paint-canvas");
 let timer
 let color = "black";
+let size = "3px";
+let isDrawing = false;
+document.addEventListener("mousedown", e => {isDrawing = true});
+document.addEventListener("mouseup", e => {isDrawing = false});
+document.addEventListener("mousemove", paint);
 
 function paint() {
-    removeListener();
-    document.addEventListener("mousedown", startPaint);
-    document.addEventListener("mouseup", stopPaint);
+    if (isDrawing === true) {
+        paintDiv(color, size)
+    }
 }
 
-function startPaint() {
-    document.addEventListener("mousemove", paintDiv);
-    setInterval(paintDiv(), 0.00005);
-}
-
-function stopPaint() {
-    document.removeEventListener("mousemove", paintDiv);
-}
-
-function paintDiv() {
+function paintDiv(paintColor, paintSize) {
     let paintCanvas = document.getElementById("paint-canvas");
     let x = event.clientX - 291;
     let y = event.clientY - 113;
     let div = document.createElement("div");
     paintCanvas.appendChild(div);
-    div.style.backgroundColor = color;
+    div.style.backgroundColor = paintColor;
     div.style.position = "absolute";
     div.style.left = x + 'px';
     div.style.top = y + 'px';
-    div.style.height = "3px";
-    div.style.width = "3px";
-}
-
-function erase() {
-    removeListener();
-    document.addEventListener("mousedown", startErase);
-    document.addEventListener("mouseup", stopErase);
-}
-
-function startErase() {
-    document.addEventListener("mousemove", eraseDiv);
-    setInterval(eraseDiv(), 0.00005);
-}
-
-function stopErase() {
-    document.removeEventListener("mousemove", eraseDiv);
-}
-
-function eraseDiv() {
-    let paintCanvas = document.getElementById("paint-canvas");
-    let x = event.clientX - 291;
-    let y = event.clientY - 113;
-    let div = document.createElement("div");
-    paintCanvas.appendChild(div);
-    div.style.backgroundColor = "white";
-    div.style.position = "absolute";
-    div.style.left = x + 'px';
-    div.style.top = y + 'px';
-    div.style.height = "15px";
-    div.style.width = "15px";
+    div.style.height = paintSize;
+    div.style.width = paintSize;
 }
 
 function changeColor(newColor) {
@@ -66,10 +33,6 @@ function changeColor(newColor) {
     colorField.style.backgroundColor = newColor;
 }
 
-function removeListener() {
-    document.removeEventListener("mousedown", startPaint);
-    document.removeEventListener("mouseup", stopPaint);
-    document.removeEventListener("mousedown", startErase);
-    document.removeEventListener("mouseup", stopErase);
-    console.log("All erased")
+function changeSize(newSize) {
+    size = newSize;
 }
