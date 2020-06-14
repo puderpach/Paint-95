c2db9ffabb235367b4c2b1071408b45b7f382cf0
Paul Schoepplenberg <paulschoepplenberg@Pauls-MacBook-Air.local>
2020-06-14 11:20:10 +0200

    Added Save/Load and changed click logic
    - added save and load buttons to use local storage
    - added the creation of one div onclick
    - changed position of divs to match mouse position
    - added forms square and circle
    - changed various small things to make forms possible

--- Paint_95.js
+++ Paint_95.js
@@ -1,19 +1,20 @@
 let paintCanvas = document.getElementById("paint-canvas");
 let timer
 let color = "black";
-let size = "3px";
+let size = 3;
 let isDrawing = false;
+let form = 0;
 document.addEventListener("mousedown", e => { isDrawing = true });
 document.addEventListener("mouseup", e => { isDrawing = false });
 document.addEventListener("mousemove", paint);
 
 function paint() {
     if (isDrawing === true) {
-        paintDiv(color, size)
+        paintDiv(color, size, form)
     }
 }
 
-function paintDiv(paintColor, paintSize) {
+function paintDiv(paintColor, paintSize, paintForm) {
     let paintCanvas = document.getElementById("paint-canvas");
     let x = event.clientX - 291;
     let y = event.clientY - 113;
@@ -21,10 +22,11 @@ function paintDiv(paintColor, paintSize) {
     paintCanvas.appendChild(div);
     div.style.backgroundColor = paintColor;
     div.style.position = "absolute";
-    div.style.left = x + 'px';
-    div.style.top = y + 'px';
-    div.style.height = paintSize;
-    div.style.width = paintSize;
+    div.style.left = (x - paintSize / 2) + 'px';
+    div.style.top = (y - paintSize / 2) + 'px';
+    div.style.height = paintSize + 'px';
+    div.style.width = paintSize + 'px';
+    div.style.borderRadius = paintForm + '%';
 }
 
 function changeColor(newColor) {
@@ -39,6 +41,17 @@ function changeSize(newSize) {
 
 function changeSizeButton() {
     let buttonSize = document.getElementById("pencil-size").valueAsNumber;
-    changeSize(buttonSize + "px");
+    changeSize(buttonSize);
 }
 
+function changeForm(radius) {
+    form = radius;
+}
+
+function savePainting() {
+    localStorage.savedPainting = document.getElementById("paint-canvas").innerHTML;
+}
+
+function loadPainting() {
+    document.getElementById("paint-canvas").innerHTML = localStorage.savedPainting;
+}
